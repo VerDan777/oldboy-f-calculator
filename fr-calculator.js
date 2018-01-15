62538b15baefe0b7ec64647b73510a46b4d4f1c9
nigel-riss <mr.kurenkov@gmail.com>
2018-01-15 14:58:38 +0200

    Add slider functionality

--- fr-calculator.js
+++ fr-calculator.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // range slider setup
     $('input[type="range"]').rangeslider({
         // Feature detection the default is `true`.
         // Set this to `false` if you want to use
@@ -18,9 +19,19 @@ $(document).ready(function() {
         onInit: function() {},
 
         // Callback function
-        onSlide: function(position, value) {},
+        onSlide: function(position, value) {
+            sliderChange(value);
+        },
 
         // Callback function
         onSlideEnd: function(position, value) {}
     });
+
+    function sliderChange(value) {
+        // var currentInput = $(".rangeslider--active").parent().children(".fr-calculator__input");
+        var currentInput = $(".rangeslider--active").parent().children(".fr-calculator__input");
+        currentInput.val(value);
+        console.log(currentInput.val());
+        console.log(value);
+    }
 });
