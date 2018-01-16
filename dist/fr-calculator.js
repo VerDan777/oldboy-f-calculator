f62f594c143dc406e59ae2fbea1520a494de51d3
nigel-riss <mr.kurenkov@gmail.com>
2018-01-16 14:31:01 +0200

    Add basic calculator functionality

--- dist/fr-calculator.js
+++ dist/fr-calculator.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-    // range slider setup
+    ////////////////////////
+    // range slider setup //
+    ////////////////////////
+
+    // swapping all input[type="range"] with polyfills
     $('input[type="range"]').rangeslider({
         // Feature detection the default is `true`.
         // Set this to `false` if you want to use
@@ -27,13 +31,15 @@ $(document).ready(function() {
         onSlideEnd: function(position, value) {}
     });
 
-    // 
+    // slider change handler
     function sliderChange(value) {
         var currentInput = $(".rangeslider--active").parent().children(".fr-calculator__input");
         currentInput.val(value);
+        setProfit();
     }
 
     // return event.charCode >= 48 && event.charCode <= 57;
+    // fields input handler
     $(".fr-calculator__input").on("keypress", function(event) {
         // check if numeral input
         var filterNumerals = event.charCode >= 48 && event.charCode <= 57;
@@ -49,4 +55,43 @@ $(document).ready(function() {
             event.preventDefault();
         };
     });
+
+    ////////////////////////
+    // profit calculation //
+    ////////////////////////
+
+    // caching fields
+    var $rent           = $("#fc-rent");
+    var $avgCheck       = $("#fc-average-check");
+    var $adsCost        = $("#fc-ads-cost");
+    var $barberShare    = $("#fc-barber-share");
+    var $clientsNum     = $("#fc-clients-num");
+    
+    function calcProfit() {
+        // getting field values
+        var rent = parseInt($rent.val());
+        var avgCheck = parseInt($avgCheck.val());
+        var adsCost = parseInt($adsCost.val());
+        var barberShare = parseInt($barberShare.val());
+        var clientsNum = parseInt($clientsNum.val());
+
+        // counting profit
+        var VISITS_PER_MONTH = 1;
+        var CLEANUP_N_OTHER = 10000;
+        var RECEPTIONIST_SALARY = 45000;
+        var income = clientsNum * avgCheck * VISITS_PER_MONTH;
+        var barbersSalary = barberShare * income / 100;
+        var shavingCosts = 30 * clientsNum * VISITS_PER_MONTH;
+
+        var profit = income - rent - CLEANUP_N_OTHER - RECEPTIONIST_SALARY - adsCost - shavingCosts - barbersSalary;
+
+        return parseInt(profit);
+    }
+
+    // setting profit field
+    function setProfit() {
+        $("#fc-profit").html(calcProfit() + " <span>рублей</span>");
+    }
+
+    setProfit();
 });
